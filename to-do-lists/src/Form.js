080430950bf427a4cd3fbc22878d116d5ad86a2e
thienan1512 <annhtts2009002@fpt.edu.vn>
2021-12-11 16:11:51 +0700

    Set-up
    set up to do list

---
+++ to-do-lists/src/Form.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './App.css';
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {task:''};
+    }
+}
+export default Form;

af653844a6db491fe61b21a4470324ffd2e85fc6
thienan1512 <annhtts2009002@fpt.edu.vn>
2021-12-12 00:12:34 +0700

    update todo
    123

--- to-do-lists/src/Form.js
+++ to-do-lists/src/Form.js
@@ -1,28 +1,69 @@
 import React from "react";
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle, faTrash } from "@fortawesome/free-solid-svg-icons";
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { tasksList: [] , task: ''};
+    this.state = {
+      tasksList: [],
+      task: "",
+      myStyle: {
+        display: "inline-block",
+        position: "relative",
+        left: "50%",
+        transform: "translateX(-50%)"
+      },
+    };
   }
+
   displayTasks(props) {
-    return <li>{props.task}</li>;
+    return (
+      <>
+        <h3>
+          <button>
+            <i className="fa fa-check" style={{ color: "green" }}></i>
+          </button>
+          <button>
+            <i className="fa fa-trash" style={{ color: "red" }}></i>
+          </button>
+          {props.task}
+        </h3>
+      </>
+    );
   }
+  
   handleTasks = (event) => {
-    this.setState({task: event.target.value});
-  }
+    this.setState({ task: event.target.value });
+  };
   addTask = () => {
     console.log(this.state.tasksList);
     this.state.tasksList.push(this.state.task);
-    this.setState({task:''});
+    this.setState({ task: "" });
   };
+  // deleteTask=()=>{
+  //   console.log("delete");
+  // }
   render() {
-    return <div className="App">
-        <h3>To do Lists</h3>     
-            <input type="text" className="Form-control" placeholder="Enter for a new Task" value={this.state.task} name="task" onChange={this.handleTasks}/>
-            <input type="button" value="Submit" onClick={this.addTask}/>
-            <ul>{this.state.tasksList.map((task)=><this.displayTasks task={task}/>)}</ul>
-    </div>;
+    return (
+      <div style={this.state.myStyle}>
+        <h3>To do Lists</h3>
+        <input
+          type="text"
+          className="Form-control"
+          placeholder="Enter for a new Task"
+          value={this.state.task}
+          name="task"
+          onChange={this.handleTasks}
+        />
+        <input type="button" value="Submit" onClick={this.addTask} />
+        <ul>
+          {this.state.tasksList.map((task) => (
+            <this.displayTasks task={task} />
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 export default Form;

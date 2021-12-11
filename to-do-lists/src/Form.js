0bd8dd5df7ac444a39cc2cf39c752f7ce95b417a
thienan1512 <annhtts2009002@fpt.edu.vn>
2021-12-11 16:17:48 +0700

    Set up
    set up tiếp

--- to-do-lists/src/Form.js
+++ to-do-lists/src/Form.js
@@ -1,9 +1,21 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {task:''};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { task: "" };
+  }
+  takeTask = (event) => {
+    this.setState({ task: event.target.value});
+    console.log("Task : "+this.state.task);
+  };
+  render() {
+    return <div className="App">
+        <h3>To do Lists</h3>
+        <form>
+            <input type="text" className="Form-control" placeholder="Enter for a new Task" onChange={this.takeTask}/>
+        </form>
+    </div>;
+  }
 }
 export default Form;

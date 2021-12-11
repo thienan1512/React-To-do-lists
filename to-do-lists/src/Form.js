3e39466298a847cbe00e3134fba8c4b043ef5039
thienan1512 <annhtts2009002@fpt.edu.vn>
2021-12-11 17:29:05 +0700

    Add to-do
    123

--- to-do-lists/src/Form.js
+++ to-do-lists/src/Form.js
@@ -3,18 +3,25 @@ import "./App.css";
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { task: "" };
+    this.state = { tasksList: [] , task: ''};
   }
-  takeTask = (event) => {
-    this.setState({ task: event.target.value});
-    console.log("Task : "+this.state.task);
+  displayTasks(props) {
+    return <li>{props.task}</li>;
+  }
+  handleTasks = (event) => {
+    this.setState({task: event.target.value});
+  }
+  addTask = () => {
+    console.log(this.state.tasksList);
+    this.state.tasksList.push(this.state.task);
+    this.setState({task:''});
   };
   render() {
     return <div className="App">
-        <h3>To do Lists</h3>
-        <form>
-            <input type="text" className="Form-control" placeholder="Enter for a new Task" onChange={this.takeTask}/>
-        </form>
+        <h3>To do Lists</h3>     
+            <input type="text" className="Form-control" placeholder="Enter for a new Task" value={this.state.task} name="task" onChange={this.handleTasks}/>
+            <input type="button" value="Submit" onClick={this.addTask}/>
+            <ul>{this.state.tasksList.map((task)=><this.displayTasks task={task}/>)}</ul>
     </div>;
   }
 }
